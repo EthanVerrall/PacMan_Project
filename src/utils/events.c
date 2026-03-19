85b6ccebae6a8282d6fbeb76e279c9db89dac4c0
Ethan Verrall <ejverrall24@gmail.com>
2026-03-19 23:23:04 +0000

    Created menu header and c file. Created events header and c file. Neatened main file by moving all STM setup functions to events. Ready for merge into test_branch.

---
+++ src/utils/events.c
@@ -0,0 +1,145 @@
+#include "../include/utils/events.h"
+#include "../include/serial.h"
+#include "../include/music&sound/sound.h"
+#include "../include/frontend&drawing/display.h"
+
+volatile uint32_t milliseconds;  //DO NOT TOUCH THIS VARIABLE, CLOCK USES THIS FOR INTERRUPTS AND TIMERS AND TICKS
+
+void SET_UP_STM() {
+
+    //Initial STM startup
+    initClock();
+	initSysTick();
+    initSerial();
+	
+    //Peripherals startup
+    setupIO();
+    initSound();
+    //Our 5 buttons go here by Peripherals
+    //Remember to turn on pull_up resistors for each button you use :)
+
+}
+
+//---------------
+//Setup functions - Start
+//---------------
+
+//DONT USE THIS OUTSIDE OF FUNCTION SET_UP_STM
+void initSysTick(void)
+{
+	SysTick->LOAD = 48000;
+	SysTick->CTRL = 7;
+	SysTick->VAL = 10;
+	__asm(" cpsie i "); // enable interrupts
+}
+
+//DONT USE THIS OUTSIDE OF FUNCTION SET_UP_STM
+void SysTick_Handler(void)
+{
+	milliseconds++;
+}
+
+//DONT USE THIS OUTSIDE OF FUNCTION SET_UP_STM
+void initClock(void)
+{
+// This is potentially a dangerous function as it could
+// result in a system with an invalid clock signal - result: a stuck system
+        // Set the PLL up
+        // First ensure PLL is disabled
+        RCC->CR &= ~(1u<<24);
+        while( (RCC->CR & (1 <<25))); // wait for PLL ready to be cleared
+
+// Warning here: if system clock is greater than 24MHz then wait-state(s) need to be
+// inserted into Flash memory interface
+
+        FLASH->ACR |= (1 << 0);
+        FLASH->ACR &=~((1u << 2) | (1u<<1));
+        // Turn on FLASH prefetch buffer
+        FLASH->ACR |= (1 << 4);
+        // set PLL multiplier to 12 (yielding 48MHz)
+        RCC->CFGR &= ~((1u<<21) | (1u<<20) | (1u<<19) | (1u<<18));
+        RCC->CFGR |= ((1<<21) | (1<<19) );
+
+        // Need to limit ADC clock to below 14MHz so will change ADC prescaler to 4
+        RCC->CFGR |= (1<<14);
+
+        // and turn the PLL back on again
+        RCC->CR |= (1<<24);
+        // set PLL as system clock source
+        RCC->CFGR |= (1<<1);
+}
+
+//Use for interrupts and sleeps - parameter is in milliseconds
+void delay(volatile uint32_t dly)
+{
+	uint32_t end_time = dly + milliseconds;
+	while(milliseconds != end_time)
+		__asm(" wfi "); // sleep
+}
+
+//Turn on pullup resistors for buttons
+void enablePullUp(GPIO_TypeDef *Port, uint32_t BitNumber)
+{
+	Port->PUPDR = Port->PUPDR &~(3u << BitNumber*2); // clear pull-up resistor bits
+	Port->PUPDR = Port->PUPDR | (1u << BitNumber*2); // set pull-up bit
+}
+
+//Set GPIOA PORT bits and their mode 
+void pinMode(GPIO_TypeDef *Port, uint32_t BitNumber, uint32_t Mode)
+{
+	/*
+	*/
+	uint32_t mode_value = Port->MODER;
+	Mode = Mode << (2 * BitNumber);
+	mode_value = mode_value & ~(3u << (BitNumber * 2));
+	mode_value = mode_value | Mode;
+	Port->MODER = mode_value;
+}
+
+//No clue what this does thb? - Ask lecturer about this one
+int isInside(uint16_t x1, uint16_t y1, uint16_t w, uint16_t h, uint16_t px, uint16_t py)
+{
+	// checks to see if point px,py is within the rectange defined by x,y,w,h
+	uint16_t x2,y2;
+	x2 = x1+w;
+	y2 = y1+h;
+	int rvalue = 0;
+	if ( (px >= x1) && (px <= x2))
+	{
+		// ok, x constraint met
+		if ( (py >= y1) && (py <= y2))
+			rvalue = 1;
+	}
+	return rvalue;
+}
+
+//Turns our LED on, 
+//DONT USE THIS OUTSIDE OF FUNCTION SET_UP_STM
+void setupIO()
+{
+	RCC->AHBENR |= (1 << 18) + (1 << 17); // enable Ports A and B
+	display_begin();
+	pinMode(GPIOB,4,0);
+	pinMode(GPIOB,5,0);
+	pinMode(GPIOA,8,0);
+	pinMode(GPIOA,11,0);
+	enablePullUp(GPIOB,4);
+	enablePullUp(GPIOB,5);
+	enablePullUp(GPIOA,11);
+	enablePullUp(GPIOA,8);
+}
+
+//---------------
+//Setup functions - End
+//---------------
+
+
+
+//---------------
+//Pacman Related functions - Start
+//---------------
+
+
+//---------------
+//Pacman Related functions - End
+//---------------
